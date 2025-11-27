6f23fac12a8332c1a6187a697b9a49d158f4e196
Alkimiya-leo <83831141+Alkimiya-leo@users.noreply.github.com>
2025-11-27 21:15:51 +0800

    Initial Commit

---
+++ gesturenoteTests/gesturenoteTests.swift
@@ -0,0 +1,17 @@
+//
+//  gesturenoteTests.swift
+//  gesturenoteTests
+//
+//  Created by EnchantedBroccoli on 11/27/25.
+//
+
+import Testing
+@testable import gesturenote
+
+struct gesturenoteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
